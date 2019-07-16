34bc3b8fb9a541226742ce1a3c241806e8f75ff0
Cyb3rAl13n <cyb3r_al13n@protonmail.com>
2019-07-16 02:29:27 -0400

    Added the deletion of posts!

--- app/models/user.rb
+++ app/models/user.rb
@@ -57,4 +57,8 @@ class User < ApplicationRecord
   def send_password_reset_email
     UserMailer.password_reset(self).deliver_now
   end
+
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
 end
--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -20,4 +20,14 @@ class MicropostsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to login_path
   end
 
+  test "should redirect destroy for wrong micropost" do
+    log_in_as(users(:one))
+    micropost = microposts(:ants)
+    assert_no_difference 'Micropost.count' do
+      delete micropost_path(micropost)
+    end
+    assert_redirected_to current_user
+  end
+
+
 end
